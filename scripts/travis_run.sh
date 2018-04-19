a752fe5b5e3b50183bc33af726c16fa042bd3542
LolHens <pierrekisters@gmail.com>
2018-04-19 09:00:19 +0200

    tests

--- scripts/travis_run.sh
+++ scripts/travis_run.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -eux
 
-ls /usr
-ls /usr/*mingw*/
-ls /usr/i586-mingw32msvc/include
-cat /usr/i586-mingw32msvc/include/errno.h
-cat /usr/x86_64-w64-mingw32/include/errno.h
+ls /usr/bin/
 
 #mvn package
 
